3737bb1d795920557d7d7aeecdb913e2fa5ce6ba
OlgaSavytska <o.savytska77@gmail.com>
2019-09-10 18:04:05 +0300

    Updates

---
+++ precache-manifest.30d0b61c5c0fc67e44729d15e8d73b81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18f08df16480b3a799264a66614ebc0",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "5d59053ca27f52299b09",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "6129f8d45508cbdbe5e8",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.3148d4ca.chunk.css"
+  },
+  {
+    "revision": "5d59053ca27f52299b09",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.88106ae8.chunk.js"
+  },
+  {
+    "revision": "6129f8d45508cbdbe5e8",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.833044b7.chunk.js"
+  },
+  {
+    "revision": "c06bd6fd29c7f3546cd3",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.5ead4510.js"
+  }
+]);
